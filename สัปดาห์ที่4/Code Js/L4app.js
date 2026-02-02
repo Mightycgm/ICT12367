0c009a10f412d2fc78a0234366a4e2f7290cc5a9
Xeroc <Nachasil.naken03@gmail.com>
2026-02-02 13:38:31 +0700

    Add Week 4 Assignment

---
+++ สัปดาห์ที่4/Code Js/L4app.js
@@ -0,0 +1,15 @@
+const smallimg = document.querySelectorAll(".gallery img")
+const fullimg = document.querySelector(".full-image")
+const modal = document.querySelector(".modal")
+smallimg.forEach(img => {
+    img.addEventListener("click", () => {
+        fullimg.src = img.src
+        modal.classList.add("open")
+    })
+})
+
+modal.addEventListener("click", (e) => {
+    if (e.target.classList.contains("modal")) {
+        modal.classList.remove("open")
+    }
+})
